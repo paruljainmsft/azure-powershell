1d4919c254003d6539e81fdc562b0909a5755b8c
Yunchi.Wang <yunwang@microsoft.com>
2025-03-19 00:28:28 +0800

    init

--- tools/PublishModules.psm1
+++ tools/PublishModules.psm1
@@ -476,7 +476,11 @@ function Save-PackageLocally {
     $AccessTokenSecureString = $env:SYSTEM_ACCESS_TOKEN | ConvertTo-SecureString -AsPlainText -Force
     $credentialsObject = [pscredential]::new("ONEBRANCH_TOKEN", $AccessTokenSecureString)
 
-    # Register-PSResourceRepository -Name $Env:DEFAULT_PS_REPOSITORY_NAME -Uri $Env:DEFAULT_PS_REPOSITORY_URL -Trusted:$True
+    if (Get-PSResourceRepository -Name $Env:DEFAULT_PS_REPOSITORY_NAME -ErrorAction SilentlyContinue) {
+        Write-Output "Repository $Env:DEFAULT_PS_REPOSITORY_NAME already registered"
+    } else {
+    Register-PSResourceRepository -Name $Env:DEFAULT_PS_REPOSITORY_NAME -Uri $Env:DEFAULT_PS_REPOSITORY_URL -Trusted:$True
+    }
     install-module $ModuleName -Repository $Env:DEFAULT_PS_REPOSITORY_NAME -Credential $credentialsObject
 
     # Only check for the modules that specifies = required exact dependency version
